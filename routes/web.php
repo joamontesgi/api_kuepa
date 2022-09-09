6172f472bd5c833ef8c67e2807b32b59c91d7cfe
joamontesgi <joamontesgi@unal.edu.co>
2022-09-08 19:29:42 -0500

    Se crean modelos y controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// create route resource
+Route::resource('pepito', ProductsController::class);
+
